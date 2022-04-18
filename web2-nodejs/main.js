f155ba001761fd5aa3e7502b9a637eef95a74df7
Kim Yu Na <yunasun0920@gmail.com>
2022-04-18 20:41:35 +0900

    [App] 파일 생성과 리다이렉션 #9

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body){
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body){
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -40,8 +42,7 @@ var app = http.createServer(function (request, response) {
     var pathname = url.parse(_url, true).pathname;
 
     if (pathname === '/') {
-      if(queryData.id === undefined){
-        
+      if(queryData.id === undefined){        
         fs.readdir('./data', function(error, filelist){
           console.log(filelist);
 
@@ -50,12 +51,11 @@ var app = http.createServer(function (request, response) {
 
           var list = templateList(filelist);          
 
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-        });
-  
+          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);  
+
           response.writeHead(200);
           response.end(template);
-        
+        });        
       }else {
         fs.readdir('./data', function(error, filelist){          
           fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -68,7 +68,47 @@ var app = http.createServer(function (request, response) {
           });
         });
       }
-    }else{
+    }else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){          
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = 'WEB - create';
+          var list = templateList(filelist);     
+          
+          var template = templateHTML(title, list, `
+            <form action="http://localhost:3000/create_process" method="post">
+              <p><input type="text" name="title" placeholder="title"></p>
+              <p>
+                <textarea name="description" placeholder="description></textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+          `);
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    }else if(pathname === '/create_process'){
+      var body = '';
+      // post 방식으로 전송된 데이터가 많을 경우 사용
+      // 서버쪽에서 수신할 때마다 콜백함수를 호출함
+      request.on('data', function(data){
+        body = body + data;
+      });
+      // 더 이상 들어올 데이터가 없을 때 콜백함수가 없을 떄 => 정보 수신이 끝났다는 것을 알 수 있음.
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var title = post.title;
+        var description = post.description;
+
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end('success');
+        })
+      });
+    }
+    else{
       response.writeHead(404);
       response.end('Not found');
     }
